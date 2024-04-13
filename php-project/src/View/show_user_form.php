3f9691fcafac5b59da7fbd2492d7d8358df052fd
cetusw <kugelev.m@gmail.com>
2024-04-13 09:51:42 +0300

    Поправил форматирование в show_user_form.php

--- php-project/src/View/show_user_form.php
+++ php-project/src/View/show_user_form.php
@@ -8,13 +8,13 @@ require_once __DIR__ . '/../Infrastructure/Utils.php';
 	<title>User info</title>
 </head>
 <body>
-<h1>User <?= htmlentities($user->getId()) ?> Information</h1>
-<span>First Name: <?= htmlentities($user->getFirstName()) ?></span><br>
-<span>Last name: <?= htmlentities($user->getLastName()) ?></span><br>
-<span>Middle name: <?= htmlentities($user->getMiddleName()) ?></span><br>
-<span>Gender: <?= htmlentities($user->getGender()) ?></span><br>
-<span>Birth Date: <?= htmlentities(Utils::convertDataTimeToString($user->getBirthDate())) ?></span><br>
-<span>Email: <?= htmlentities($user->getEmail()) ?></span><br>
-<span>Phone:<?= htmlentities($user->getPhone()) ?></span><br>
-<span>Avatar path: <?= htmlentities($user->getAvatarPath()) ?></span>
+    <h1>User <?= htmlentities($user->getId()) ?> Information</h1>
+    <span>First Name: <?= htmlentities($user->getFirstName()) ?></span><br>
+    <span>Last name: <?= htmlentities($user->getLastName()) ?></span><br>
+    <span>Middle name: <?= htmlentities($user->getMiddleName()) ?></span><br>
+    <span>Gender: <?= htmlentities($user->getGender()) ?></span><br>
+    <span>Birth Date: <?= htmlentities(Utils::convertDataTimeToString($user->getBirthDate())) ?></span><br>
+    <span>Email: <?= htmlentities($user->getEmail()) ?></span><br>
+    <span>Phone:<?= htmlentities($user->getPhone()) ?></span><br>
+    <span>Avatar path: <?= htmlentities($user->getAvatarPath()) ?></span>
 </body>
